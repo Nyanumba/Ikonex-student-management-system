e2616f6e3732384c4ebfa9b58ee6bd08a0974e9d
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2026-06-07 05:00:43 +0300

    added api endpoints for adding students and subjects

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const db = require("./models");
 
+
 dotenv.config();
 
 const app = express();
@@ -10,8 +11,24 @@ app.use(express.json());
 
 // ROUTES
 const authRoutes = require("./routes/authRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const classStreamRoutes = require("./routes/classStreamRoutes");
+const subjectRoutes = require("./routes/subjectRoutes");
+const scoreRoutes = require("./routes/scoreRoutes");
+const rankingRoutes = require("./routes/rankingRoutes");
 
 app.use("/api/auth", authRoutes);
+
+
+app.use("/api/students", studentRoutes);
+
+app.use("/api/class-streams", classStreamRoutes);
+
+app.use("/api/subjects", subjectRoutes);
+
+app.use("/api/scores", scoreRoutes);
+
+app.use("/api/rankings", rankingRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running");
